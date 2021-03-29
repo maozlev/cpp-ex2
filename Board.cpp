ac9656ac490b3f6eb554824544b4d64945b0f2c3
maoz lev <maoz20011111@gmail.com>
2021-03-29 21:56:37 +0300

    update

--- Board.cpp
+++ Board.cpp
@@ -4,6 +4,8 @@ using ariel::Direction;
 
 #include <iostream>
 #include <vector>
+// #include <string>
+#include <iostream>
 #include <stdexcept>
 #include <algorithm>
 using namespace std;
@@ -13,37 +15,38 @@ namespace ariel{
     void Resize_Board_rows(vector<vector<char>> &Board, unsigned int row, unsigned int col);
     void Resize_Board_cols(vector<vector<char>> &Board, unsigned int row, unsigned int col);
     
+    void Resize_Board_rows(vector<vector<char>> &Board, unsigned int row, unsigned int col ){
+        vector<char> v (col);
+        fill(v.begin(), v.end(), '_');
+        for (unsigned int  i = 0; i < row; i++){
+            Board.push_back(v);
+        }   
+    }
+
+    void Resize_Board_cols(vector<vector<char>> &Board, unsigned int row, unsigned int col ){
+         unsigned int size = Board[0].size();
+        for (unsigned int i = 0; i < col; i++){   
+            Board[i].resize(row);
+            fill(Board[i].begin()+size, Board[i].end(),'_');
+        }
+    }
+
     Board::Board(){
-        flag = 0;
+       vector<char> tmp{};
+       tmp.push_back('_');
+       Main_Board.push_back(tmp);
     }
 
     void Board::post(unsigned int row, unsigned int col, Direction d, string message ){
-        if(flag == 0){ // init the board
-            flag = 1;
-            vector<char> v;
-            if(d == Direction::Horizontal){
-                vector<char> v (col + message.length());
-                fill(v.begin(), v.end(), '_');
-                for (unsigned int  i = 0; i < row + message.length(); i++){
-                    Main_Board.push_back(v);
-                }
-            }
-            if(d == Direction::Vertical){
-                vector<char> v (col);
-                fill(v.begin(), v.end(), '_');
-                for (unsigned int  i = 0; i < row + message.length() ; i++){
-                    Main_Board.push_back(v);
-                }   
-            }
-        }
+        
         if(d == Direction::Horizontal){
 
-            if(row > Main_Board.size()){
-                Resize_Board_rows(Main_Board, row - Main_Board.size(),message.length() ); //Main_Board[0].size()
+            if(row >= Main_Board.size()){
+                Resize_Board_rows(Main_Board, row+2 - Main_Board.size(),Main_Board[0].size()); 
             }
             if(col + message.length() > Main_Board[0].size()){
-                Resize_Board_cols(Main_Board, Main_Board.size(), col + message.length());}
-
+                Resize_Board_cols(Main_Board, col+message.length(), Main_Board.size());
+            }
             unsigned int size_string = 0;
             for (unsigned int i = col; i < col + message.length(); i++){
                 Main_Board[row][i] = message[size_string];
@@ -55,8 +58,8 @@ namespace ariel{
             if(row + message.length()> Main_Board.size()){
                 Resize_Board_rows(Main_Board, row + message.length() - Main_Board.size(), Main_Board[0].size());}
             
-            if(col > Main_Board[0].size()){
-                Resize_Board_cols(Main_Board, Main_Board.size(), col);}
+            if(col >= Main_Board[0].size()){
+                Resize_Board_cols(Main_Board, col+2, Main_Board.size());}
             
             unsigned int size_string = 0;
             for (unsigned int i = row; i < row + message.length(); i++){
@@ -67,28 +70,27 @@ namespace ariel{
     }
 
     string Board::read(unsigned int row, unsigned int col, Direction d, unsigned int len ){
-        int add_to_word;
         string message_out;
         if(d == Direction::Horizontal){
-            add_to_word = len+col-Main_Board[0].size();
-
+            if(row>=Main_Board.size()){
+                Resize_Board_rows(Main_Board, row+1-Main_Board.size(), Main_Board[0].size());        
+            }
+            if(col+len>Main_Board[0].size()){
+                Resize_Board_cols(Main_Board, col+len, Main_Board.size());
+            }
             for (unsigned int i = col; i < col + len ; i++){
                 message_out+=Main_Board[row][i];
             }
-            while(add_to_word > 0){
-                message_out += '_';
-                add_to_word--;
-            }
         }
-    
         else if(d == Direction::Vertical){
-            add_to_word = len+row-Main_Board.size();
+            if(row+len>Main_Board.size()){
+                Resize_Board_rows(Main_Board, row+len-Main_Board.size(), Main_Board[0].size());        
+            }
+            if(col>=Main_Board[0].size()){
+                Resize_Board_cols(Main_Board, col+1, Main_Board.size());
+            }
             for (unsigned int i = row; i < row + len; i++){
                 message_out += Main_Board[i][col];
-            }
-            while(add_to_word>0){
-                message_out+='_';
-                add_to_word--;
             }
         }
         return message_out;
@@ -104,19 +106,5 @@ namespace ariel{
 	    }
     }
 
-     void Resize_Board_rows(vector<vector<char>> &Board, unsigned int row, unsigned int col ){
-        vector<char> v (col);
-        fill(v.begin(), v.end(), '_');
-        for (unsigned int  i = 0; i < row; i++){
-            Board.push_back(v);
-        }   
-    }
-
-    void Resize_Board_cols(vector<vector<char>> &Board, unsigned int row, unsigned int col ){
-        for (unsigned int i = 0; i < row; i++){
-            unsigned int size = Board[i].size();
-            Board[i].resize(col);
-            fill(Board[i].begin()+size, Board[i].end(),'_');
-        }
-    }
+   
 }
